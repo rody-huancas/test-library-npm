752613db6660abf530697134c721661eb8b8172d
Rody Huancas <rodyhuancas.04@gmail.com>
2024-12-12 22:23:47 -0500

    Feat: component polimorfic

---
+++ src/Text/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Text } from "./Text";
+
+const meta: Meta<typeof Text> = {
+  title: "Components/Text",
+  component: Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Text>;
+
+export const Default: Story = {
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti a architecto eveniet ipsam? Obcaecati architecto, quia voluptate ea earum cupiditate. Expedita, accusantium. Eum omnis error fuga accusamus, iste iusto voluptatum!",
+  },
+};
